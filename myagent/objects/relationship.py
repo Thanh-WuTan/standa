1ac04c3c927632369f41aaa01b2ad431094468a3
Thanh-WuTan <wt.vathanh@gmail.com>
2024-09-18 10:05:47 +0000

    feat: add property shorthand

--- myagent/objects/relationship.py
+++ myagent/objects/relationship.py
@@ -4,3 +4,12 @@ class Relationship:
             self.edge = edge
             self.target = target
             self.score = score
+    @property
+    def shorthand(self):
+        # compute a visual representation of a relationship for recording purposes
+        stub = f"{self.source.name}({self.source.value})"
+        if self.edge:
+            stub += f" : {self.edge}"
+            if self.target and self.target.name:
+                stub += f" : {self.target.name}({self.target.value})"
+        return stub
