73094f2c70ded0eb83bd95b2fc6ba2390af04ea7
Thanh-WuTan <wt.vathanh@gmail.com>
2024-09-18 11:12:30 +0000

    feat: add: update score

--- myagent/objects/fact.py
+++ myagent/objects/fact.py
@@ -16,16 +16,25 @@ escape_ref = {
 }
 
 class Fact:
-     def __init__(self, trait, value = None, score = 1, relationships = []):
-          self.trait = trait
-          self.value = value
-          self.score = score
-          self.relationships = relationships  
+    def __init__(self, trait, value = None, score = 1, relationships = []):
+        self.trait = trait
+        self.value = value
+        self.score = score
+        self.relationships = relationships  
 
-     def escaped(self, executor):
-          if executor not in escape_ref:
-               return self.value
-          escaped_value = str(self.value)
-          for char in escape_ref[executor]['special']:
-               escaped_value = escaped_value.replace(char, (escape_ref[executor]['escape_prefix'] + char))
-          return escaped_value
+    @staticmethod
+    def hash(s):
+        return s 
+     
+    @property
+    def unique(self):
+        return self.hash('%s%s' % (self.trait, self.value))
+
+
+    def escaped(self, executor):
+        if executor not in escape_ref:
+            return self.value
+        escaped_value = str(self.value)
+        for char in escape_ref[executor]['special']:
+            escaped_value = escaped_value.replace(char, (escape_ref[executor]['escape_prefix'] + char))
+        return escaped_value
--- myagent/objects/link.py
+++ myagent/objects/link.py
@@ -26,7 +26,7 @@ class Link:
         return p_inst.parse(blob=blob)
 
     def parse(self, result, source = None):
-        source_facts = list(source.facts) if source else []
+        source_facts = list(source.facts) if source else self.facts
         for parser in self.executor.parsers:
             try:
                 relationships = self._parse_link_result(result, parser, source_facts)
@@ -34,18 +34,18 @@ class Link:
                     relationships = []  # Reset relationships if the parser signals failure
                 else:
                     self.create_relationships(relationships, source)
-            #     update_scores(operation, increment=len(relationships), used=self.used, facts=self.facts)
+                self.update_scores(increment=len(relationships), used=self.used, facts=self.facts, source = source)
             except Exception as e:
                 print("Error in %s while parsing ability %s: %s" % (parser['module'], self.ability['ability_id'], e))
 
-    def create_relationships(self, relationships, source = None):
+    def create_relationships(self, relationships, source):
         for relationship in relationships:
             self.save_fact(relationship.source, relationship.score, relationship.shorthand, source)
             self.save_fact(relationship.target, relationship.score, relationship.shorthand, source)
             if all((relationship.source.trait, relationship.edge)):
                 self.relationships.append(relationship)
 
-    def save_fact(self, fact, score, relationship, source = None):
+    def save_fact(self, fact, score, relationship, source):
         all_facts = source.facts if source else self.facts
         rl = [relationship] if relationship else []
         if all([fact.trait, fact.value]):
@@ -67,7 +67,17 @@ class Link:
                 existing_local_record = [x for x in self.facts if x.trait == fact.trait and x.value == fact.value]
                 if not existing_local_record:
                     self.facts.append(existing_fact)
-        source.facts = all_facts
+        if source:
+            source.facts = all_facts
 
+    def update_scores(self, increment, used, facts, source):
+        for uf in used:
+            
+            for found_fact in source.facts:
+                if found_fact.unique == uf.unique:
+                    found_fact.score += increment
+                    print("?!?!?!?: ", end=' ')
+                    print(found_fact.trait, found_fact.value, found_fact.score)
+                    break 
 
                 
