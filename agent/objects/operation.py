e01c507b34d3c8fa2384afaeeb059110145aed90
root <wt.vathanh@gmail.com>
2024-09-25 07:40:27 +0000

    remove spaces

--- agent/objects/operation.py
+++ agent/objects/operation.py
@@ -3,7 +3,7 @@ import time
 from main import PAYLOAD_DIR
 from objects.base_planning import *
 from objects.executor import *
-from objects.link import *
+from objects.link import * 
 
 
 class Operation:
@@ -22,54 +22,36 @@ class Operation:
     def all_relationships(self):
         return self.source.relationships
 
-
     def run(self):
+        procedures = []
         bps = BasePlanningService()
         agent = self.agents[0]
         source = self.source
-        learner = self.learner
-
-        for ability in self.adversary.abilities:
+        learner = self.learner 
+        for order, ability in enumerate(self.adversary.abilities, start=1):
             if not agent.is_capable_to_run(ability):
                 continue
             executors = agent.find_executors(ability)
             links = []  
-
             for ex in executors:
                 executor = Executor(name = ex['name'], platform = ex['platform'], command = ex['command'],
                                     parsers = ex['parsers'], timeout = ex['timeout'], payloads = ex['payloads'])
-                
                 ex_link = Link(command=ex['command'], ability=ability, executor=executor, paw=agent.paw)
-                
                 valid_links = bps.add_test_variants(links=[ex_link], agent=agent, facts=source.facts, rules=self.rules,
                                                     trim_unset_variables=True, trim_missing_requirements=True,
                                                     operation=self)
-                
                 if valid_links:
                     links = bps.sort_links(valid_links)
                     break
-
             ran_command = set()
-            for link in links:
+            procedure = []
+            for step_order, link in enumerate(links, start=1):
                 ex = link.executor
                 ex.command = ex.replace_payload_dir(link.command, PAYLOAD_DIR)
                 if ex.command in ran_command:
                     continue
                 ran_command.add(ex.command)
-
-                print("+" * 40)
-                print(f"Executing Command: {ex.command}")
-                print("+" * 40)
-
                 stdout, stderr = ex.run_command()
                 learner._save(link, stdout, stderr, self)
-                
-                print(f"\n{'='*40}")
-                print(f"Command Executed Successfully!")
-                print(f"{'-'*40}")
-                print(f"Stdout:\n{stdout.strip() if stdout else '<No Output>'}")
-                print(f"{'-'*40}")
-                print(f"Stderr:\n{stderr.strip() if stderr else '<No Errors>'}")
-                print(f"{'='*40}\n")
-
-                time.sleep(2)
+            
+            procedures.append(procedure)
